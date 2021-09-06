5aa3f62569051de1713fa4336f65bd3edee0fc04
f5joaodanieldeliz <88680833+f5joaodanieldeliz@users.noreply.github.com>
2021-09-06 16:19:11 -0300

    atividade5.py

---
+++ atividade5.py
@@ -0,0 +1,9 @@
+n1 = input("digite um nome: ")
+n2 = input("digite um nome: ")
+n3 = input("digite um nome: ")
+lista = [n1,n2,n3]
+
+if "joao" in lista:
+    print("Sim, está na lista!")
+else:
+    print("Não está na lista")
