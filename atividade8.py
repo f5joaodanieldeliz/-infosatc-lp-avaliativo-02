bd1750f2f6ca4773cacd7878d822012fa74e1b83
f5joaodanieldeliz <88680833+f5joaodanieldeliz@users.noreply.github.com>
2021-09-06 16:55:45 -0300

    atividade8.py

---
+++ atividade8.py
@@ -0,0 +1,4 @@
+lista = ["GeForce RTX 2080Ti","Gabinete Rise Mode Glass 06, Frente e Lateral em Vidro, Preto - RM-CA-06-FB","COOLER PARA PROCESSADOR PICHAU CORAX C/2 FANS LED BRANCO, PG-CR2-WHITE"]
+lista.remove("GeForce RTX 2080Ti")
+lista.remove("Gabinete Rise Mode Glass 06, Frente e Lateral em Vidro, Preto - RM-CA-06-FB")
+print(lista)
